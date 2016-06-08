b6dcca168ee1e8c1d266a393f3b6312f447ba2f8
BrianAdams <brian@babastik.com>
2016-06-08 09:59:28 -0700

    test

--- build.sh
+++ build.sh
@@ -12,8 +12,7 @@ pkg-config \
 wget \
 libpcre3 \
 libpcre3-dev \
-<<<<<<< HEAD
-dpkg-dev 
+dpkg-dev
 
 #since we are extending an exisitng source, we don't have to add keys or anything
 echo "deb http://httpredir.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list
@@ -56,34 +55,4 @@ dpkg-buildpackage -b
 mv nginx-common_* ..
 #mkdir -p ../nginx_install
 #make install DESTDIR=../nginx_install/usr/local
-=======
-libssl-dev
 
-wget http://nginx.org/download/nginx-${nginxVersion}.tar.gz
-tar xf nginx-${nginxVersion}.tar.gz
-cd nginx-${nginxVersion}
-./configure \
-    --prefix=/usr/share/nginx \
-    --sbin-path=/usr/sbin/nginx \
-    --conf-path=/etc/nginx/nginx.conf \
-    --pid-path=/var/run/nginx.pid \
-    --lock-path=/var/lock/nginx.lock \
-    --error-log-path=/var/log/nginx/error.log \
-    --http-log-path=/var/log/nginx/access.log \
-    --user=www-data \
-    --group=www-data \
-    --without-mail_pop3_module \
-    --without-mail_imap_module \
-    --without-mail_smtp_module \
-    --without-http_fastcgi_module \
-    --without-http_uwsgi_module \
-    --without-http_scgi_module \
-    --without-http_memcached_module \
-    --with-http_ssl_module \
-    --with-http_stub_status_module \
-    --with-http_gzip_static_module \
-    --with-http_v2_module
-
-mkdir -p ../nginx_install
-make install DESTDIR=../nginx_install
->>>>>>> origin/master
